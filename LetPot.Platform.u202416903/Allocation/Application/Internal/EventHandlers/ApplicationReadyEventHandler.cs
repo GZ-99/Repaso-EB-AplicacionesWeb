059748530b56da24d6b389f0a5580bb5b467c7f4
GZ-99 <gabrielvilchezvite@gmail.com>
2026-07-14 23:12:32 -0500

    chore: add ApplicationReadyEvent.cs and ApplicationReadyEventHandler.cs

---
+++ LetPot.Platform.u202416903/Allocation/Application/Internal/EventHandlers/ApplicationReadyEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using LetPot.Platform.u202416903.Allocation.Application.CommandServices;
+using LetPot.Platform.u202416903.Allocation.Domain.Model.Commands;
+using LetPot.Platform.u202416903.Allocation.Domain.Model.Events;
+using LetPot.Platform.u202416903.Shared.Application.Internal.EventHandlers;
+
+namespace LetPot.Platform.u202416903.Allocation.Application.Internal.EventHandlers;
+
+public class ApplicationReadyEventHandler(IPotCommandService potCommandService) : IEventHandler<ApplicationReadyEvent>
+{
+    public Task Handle(ApplicationReadyEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return On(domainEvent, cancellationToken);
+    }
+    
+    private async Task On(ApplicationReadyEvent domainEvent, CancellationToken  cancellationToken)
+    {
+        var seedCommand = new SeedPotsCommand();
+        await potCommandService.Handle(seedCommand, cancellationToken);
+    }
+}
---
+++ LetPot.Platform.u202416903/Allocation/Domain/Model/Events/ApplicationReadyEvent.cs
@@ -0,0 +1,8 @@
+﻿using LetPot.Platform.u202416903.Shared.Domain.Model.Events;
+
+namespace LetPot.Platform.u202416903.Allocation.Domain.Model.Events;
+
+public class ApplicationReadyEvent : IEvent
+{
+    
+}
