e250b9b4b7aad61fd1d2b5d37e24698238dd37b5
GZ-99 <gabrielvilchezvite@gmail.com>
2026-07-16 17:59:11 -0500

    chore: add new items.

---
+++ LetPot.Platform.u202416903/Telemetry/Interfaces/Acl/IPotContextFacade.cs
@@ -0,0 +1,14 @@
+﻿namespace LetPot.Platform.u202416903.Telemetry.Interfaces.Acl;
+
+public interface IPotContextFacade
+{
+    /*Task<int> CreatePot(
+        string macAddress,
+        int customerId,
+        double preferredHumidityLevel,
+        CancellationToken cancellationToken
+    );*/
+    
+    Task<int> FetchPotById(string macAddress, 
+        CancellationToken cancellationToken);
+}
